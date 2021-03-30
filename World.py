ad9d37011a90af0201db447a9e3b322baff48524
puddydoo <30945029+puddydoo@users.noreply.github.com>
2021-03-30 16:47:41 -0400

    Remove savewarp logic
    I don't think it's necessary

--- World.py
+++ World.py
@@ -379,10 +379,6 @@ class World(object):
                     logging.getLogger('').debug('Dropping unreachable exit: %s', new_exit.name)
                 else:
                     new_region.exits.append(new_exit)
-        if 'savewarp' in region:
-            new_exit = Entrance('%s -> %s' % (new_region.name, region['savewarp']), new_region)
-            new_exit.connected_region = region['savewarp']
-            new_region.exits.append(new_exit)
 
 
     def create_internal_locations(self):
